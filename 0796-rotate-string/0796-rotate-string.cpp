6f7179028f122e40a13e006a74ef40373b3d4696
rithvikkatpelly <rrkatpelly@ucdavis.edu>
2025-12-16 15:01:35 -0800

    Time: 0 ms (100%), Space: 8.3 MB (24.55%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size()) return false;
+        return (s + s).find(goal) != string::npos;
+    }
+};
