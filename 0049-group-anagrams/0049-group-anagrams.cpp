c3069392647d49a0c5549c9305beb3f8845d80f4
rithvikkatpelly <rrkatpelly@ucdavis.edu>
2025-12-18 10:18:44 -0800

    Time: 10 ms (92.63%), Space: 23.8 MB (93.33%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for (const auto& s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            groups[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto& entry : groups) {
+            result.push_back(move(entry.second));
+        }
+        return result;
+    }
+};
