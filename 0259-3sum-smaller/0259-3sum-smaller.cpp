6d052471609b25af770a6df97df44165941d964c
rithvikkatpelly <rrkatpelly@ucdavis.edu>
2025-12-18 15:20:21 -0800

    Time: 15 ms (74.37%), Space: 17.9 MB (59.97%) - LeetHub

---
+++ 0259-3sum-smaller/0259-3sum-smaller.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        int n = nums.size();
+        int count = 0;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < target) {
+                    // All triplets between left and right are valid
+                    count += (right - left);
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return count;
+    }
+};
