687cb6bcf7bceda68e392d0a4c4726ab37c65ca1
rithvikkatpelly <rrkatpelly@ucdavis.edu>
2025-12-18 10:17:25 -0800

    Time: 59 ms (41.75%), Space: 15.7 MB (62.49%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+
+        dp[0][0] = true; // empty string and empty pattern
+
+        // Initialize dp[0][j] for patterns like "*", "**", etc.
+        for (int j = 1; j <= n; ++j) {
+            if (p[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    // * matches zero or more characters
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else if (p[j - 1] == '?' || s[i - 1] == p[j - 1]) {
+                    // match exact or ?
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
