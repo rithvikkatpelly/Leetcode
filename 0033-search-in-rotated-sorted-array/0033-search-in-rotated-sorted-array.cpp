df9a5d045f3807773d8f86f090153f8eae1052da
rithvikkatpelly <rrkatpelly@ucdavis.edu>
2025-12-20 19:12:37 -0800

    Time: 0 ms (100%), Space: 15.1 MB (94.51%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // Left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;      // target in left half
+                } else {
+                    left = mid + 1;       // target in right half
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;       // target in right half
+                } else {
+                    right = mid - 1;      // target in left half
+                }
+            }
+        }
+
+        return -1; // not found
+    }
+};
