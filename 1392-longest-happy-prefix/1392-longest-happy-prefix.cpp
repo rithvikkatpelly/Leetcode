4e40e6ef2a96ee1c8318c7af04ba7a86c66e58fa
rithvikkatpelly <rrkatpelly@ucdavis.edu>
2025-12-23 15:53:45 -0800

    Time: 10 ms (40.15%), Space: 23.4 MB (29.28%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n = (int)s.size();
+        vector<int> lps(n, 0);
+
+        for (int i = 1; i < n; i++) {
+            int j = lps[i - 1];
+            while (j > 0 && s[i] != s[j]) j = lps[j - 1];
+            if (s[i] == s[j]) j++;
+            lps[i] = j;
+        }
+
+        return s.substr(0, lps[n - 1]);
+    }
+};
