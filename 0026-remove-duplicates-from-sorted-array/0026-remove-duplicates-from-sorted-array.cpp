05aa065baf3ded78cf5eb06213f36afc214434d2
Rithvikh Katpelly <rrkatpelly@ucdavis.edu>
2026-01-20 08:50:21 -0800

    Time: 0 ms (100%), Space: 22.5 MB (78.22%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        int k = 1; // position to place next unique
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
