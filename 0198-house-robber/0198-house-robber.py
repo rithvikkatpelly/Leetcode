bcaa51652d893b405084347e049ea6561c258f5d
Rithvikh Katpelly <rrkatpelly@ucdavis.edu>
2026-07-24 16:00:46 -0700

    Time: 0 ms (100%), Space: 19.3 MB (23.37%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev, curr = 0, 0
+        for num in nums:
+            prev, curr = curr, max(curr, prev + num)
+        return curr 
