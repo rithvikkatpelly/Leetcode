9c8e55ab79fb1437d8bf84f9e15e3fcd0f966738
Rithvikh Katpelly <rrkatpelly@ucdavis.edu>
2026-08-08 12:02:42 -0700

    Time: 99 ms (47.95%), Space: 28.7 MB (37.14%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []  # stores indices of days with unresolved temperatures
+        
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+            stack.append(i)
+        
+        return answer
